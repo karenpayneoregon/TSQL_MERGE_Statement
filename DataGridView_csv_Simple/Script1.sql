e9dea675719dd0dd2f055e718808e359f7c1df24
Karen Payne <paynekaren@comcast.net>
2022-12-25 03:17:35 -0800

    query added

--- DataGridView_csv_Simple/Script1.sql
+++ DataGridView_csv_Simple/Script1.sql
@@ -1,4 +1,12 @@
-﻿--- place sql here
+﻿--- for writing article
+
+SELECT Id,
+       FirstName,
+       LastName,
+       Gender,
+       BirthDay
+FROM dbo.Person;
+
 
 SELECT name FROM tempdb.sys.objects WHERE name LIKE N'#TmpPersonTable%';
 
